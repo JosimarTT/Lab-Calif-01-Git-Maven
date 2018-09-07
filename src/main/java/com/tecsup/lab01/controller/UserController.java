3e191ba47e5a3b60b782f51b0cfc51b023f64f46
Josimar <josimar.tantahuilca@tecsup.edu.pe>
2018-09-07 17:04:03 -0500

    agregado funcionalidad UserController

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -1,5 +1,16 @@
 package com.tecsup.lab01.controller;
+import java.util.Scanner;
 
-public class UserController {
+import com.tecsup.lab01.model.User;
 
+public class UserController extends User{
+	
+	public static void main( String[] args )
+	    {
+		User user = new User ();
+System.out.println("Nombre: "+user.nombre);
+   System.out.println("Apellido paterno: "+user.apellido);
+     System.out.println("Edad: "+user.edad);
+	    }
+	
 }
--- src/main/java/com/tecsup/lab01/model/User.java
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -1,5 +1,7 @@
 package com.tecsup.lab01.model;
 
 public class User {
-
+	public String nombre = "Jean Lui";
+	public String apellido = "Ferrer Rupaylla";
+	public int edad = 20;
 }
